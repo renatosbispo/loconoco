89a487b4bfba7c4cce798b88e9a3d8cc9d33cd06
Renato Bispo <renatosilvabispo@outlook.com>
2025-01-04 19:32:50 +0000

    make database.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -10,10 +10,42 @@ async function query(queryObject) {
   });
 
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
 
-  return result;
+  try {
+    const result = await client.query(queryObject);
+
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
-export default { query };
+async function getConnectionsMax() {
+  const { rows } = await query("SHOW max_connections;");
+
+  return parseInt(rows[0].max_connections);
+}
+
+async function getConnectionsOpened(dbName) {
+  const { rows } = await query({
+    text: "SELECT COUNT(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+
+  return parseInt(rows[0].count);
+}
+
+async function getServerVersion() {
+  const { rows } = await query("SHOW server_version;");
+
+  return rows[0].server_version;
+}
+
+export default {
+  getConnectionsMax,
+  getConnectionsOpened,
+  getServerVersion,
+  query,
+};
